eccb96b16fcf6a09decce552320eeae1b2f67614
Jeongeum <wjddma1202@naver.com>
2023-04-18 15:38:40 +0900

    feat : 체크박스 체크 시 상태 바뀌게 구현 및 임시 스타일 적용

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,41 +1,78 @@
 import React from "react";
-import { BsFillPencilFill } from "react-icons/bs";
-import { FaTrash } from "react-icons/fa";
+import "./TodoItem.css";
 // key를 list에서 props로 받아오면 안된다. props로 각 item을 받아와서 여기에서 펼쳐줘야한다.
 export default function TodoItem({
+  todoList,
+  setTodoList,
   text,
-  isComplate,
+  isComplete,
   isUpdating,
   item,
-  onRemove,
-  onEdit,
-  onEnterEdit,
 }) {
   const { id } = item;
+  console.log(todoList);
+
+  // 수정 버튼 클릭
+  const onEdit = (id) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, isUpdating: !todo.isUpdating } : todo
+      )
+    );
+  };
+
+  // 수정 후 엔터
+  const onEnterEdit = (e, id) => {
+    console.log(todoList);
+    if (e.key === "Enter") {
+      setTodoList(
+        todoList.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, text: e.target.value, isUpdating: false };
+          }
+          return todo;
+        })
+      );
+    }
+  };
+
+  // 체크박스 체크
+  const checkToggle = (id) => {
+    console.log("체크");
+    setTodoList(
+      todoList.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !isComplete };
+        }
+        return todo;
+      })
+    );
+  };
+
+  // 삭제
+  const onRemove = (id) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  };
   return (
     <>
       <li key={id}>
-        <div>
-          <input type="checkbox" />
-        </div>
+        <span
+          className={isComplete ? "checked" : ""}
+          onClick={() => checkToggle(id)}
+        >
+          {isComplete ? "◼" : "◻"}
+        </span>
+
         {isUpdating ? (
           <input type="text" onKeyDown={(e) => onEnterEdit(e, id)}></input>
         ) : (
-          text
+          <span className={isComplete ? "checked checkedText" : ""}>
+            {text}
+          </span>
         )}
 
-        {isUpdating ? (
-          <></>
-        ) : (
-          <button onClick={() => onEdit(id)}>
-            수정
-            {/* <BsFillPencilFill /> */}
-          </button>
-        )}
-        <button onClick={() => onRemove(id)}>
-          삭제
-          {/* <FaTrash /> */}
-        </button>
+        {isUpdating ? <></> : <button onClick={() => onEdit(id)}>수정</button>}
+        <button onClick={() => onRemove(id)}>삭제</button>
       </li>
     </>
   );
