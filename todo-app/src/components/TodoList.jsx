c520c1a52cf39fdbd3de0359d9af077b9e9cf262
Jeongeum <wjddma1202@naver.com>
2023-04-27 14:54:29 +0900

    design : todolist 컴포넌트 스타일 적용

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import "./TodoList.css";
 
 export default function TodoList({ todoList, setTodoList }) {
   return (
-    <ul>
-      {todoList.map((item) => (
-        <TodoItem
-          key={item.id}
-          todoList={todoList}
-          setTodoList={setTodoList}
-          text={item.text}
-          isComplete={item.isComplete}
-          isUpdating={item.isUpdating}
-          item={item}
-        />
-      ))}
-    </ul>
+    <div className="todolistWrapper">
+      <ul className="todolists">
+        {todoList.map((item) => (
+          <TodoItem
+            key={item.id}
+            todoList={todoList}
+            setTodoList={setTodoList}
+            text={item.text}
+            isComplete={item.isComplete}
+            isUpdating={item.isUpdating}
+            item={item}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
